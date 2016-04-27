b1ab5d0e17e5ee7d7d6cb13d4eed09bfa0c6c1b9
MortaPlyPly <gupijazaure@gmail.com>
2016-04-27 20:54:43 +0300

    raaadaauuu

--- FUQ2/src/main/webapp/js/slideshow.js
+++ FUQ2/src/main/webapp/js/slideshow.js
@@ -25,21 +25,21 @@ $(document).ready(function () {
     var img_array = [1, 2, 3, 4],
         newIndex = 0,
         index = 0,
-        interval = 4000;
+        interval = 5000;
     (function changeBg() {
 
         //  --------------------------
         //  For random image rotation:
         //  --------------------------
 
-              newIndex = Math.floor(Math.random() * 10) % img_array.length;
-              index = (newIndex === index) ? newIndex -1 : newIndex;
+              //newIndex = Math.floor(Math.random() * 10) % img_array.length;
+              //index = (newIndex === index) ? newIndex -1 : newIndex;
 
         //  ------------------------------
         //  For sequential image rotation:
         //  ------------------------------
 
-            //index = (index + 1) % img_array.length;
+            index = (index + 1) % img_array.length;
 
         $('body').css('backgroundImage', function () {
             $('#fullPage').animate({
@@ -49,7 +49,7 @@ $(document).ready(function () {
                     $('#fullPage').animate({
                         backgroundColor: 'rgb(255,255,255)'
                     }, 1000);
-                }, 2000);
+                }, 3000);
             });
             return 'url(../img/backgrounds/b.' + img_array[index] + '.jpg)';
         });
