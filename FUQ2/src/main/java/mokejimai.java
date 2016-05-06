a4a53e521def6ed83bbb72930aadb8b30be1b95f
MortaPlyPly <gupijazaure@gmail.com>
2016-05-06 19:01:56 +0300

    :(  bus sunku su tuo pdf'u

---
+++ FUQ2/src/main/java/mokejimai.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Iron Frog
+ */
+public class mokejimai {
+    
+    /*
+    MOKEJIMAI FUNKCIJOS:
+    >patikrinti suzymetas varneles (is xhtml)
+    >sugeneruoti pdf
+    >show in new window & activate "back button"
+    ------
+    >kai mygtukas payed pushed
+    is tam tikro xhtml su listu nariu paiimti info, kurios varneles pazymetos
+    >ar tikrai? -yes/no: return with emtied
+                         return with nothing changed
+    >gali atzymeti (mygtukas atzymejimui kitoj vietoj? kaip patogiau?)
+    -------
+    duombaze: narys dar turi vieta su "payed" boolean
+    */
+    
+    /*
+    PDF GENERAVIMAS IR SAUGOJIMAS:
+    
+    */
+    
+    /*
+    XHTML PAKEITIMAI:
+    
+    */
+    
+    
+}
